e6d046a4520a1f35c0228c6b88459c0ca9b3de26
kakmond <wongsathorn.pa@ku.th>
2019-04-28 01:41:56 +0700

    [Updated]: corrected API_URL

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,5 +1,5 @@
 export default {
-    API_URL: 'https://uninews-api.firebaseapp.com/api/',
+    API_URL: 'https://uninews-api.firebaseapp.com/api',
     PRIMARY_COLOR: 'tomato',
     SECONDARY_COLOR: '#24A049',
     WHITE_COLOR: '#fff'
