ef7bb7fe34f6cf9e6e7e9bbbb181daa97cf116ad
kakmond <wongsathorn.pa@ku.th>
2019-05-05 17:57:07 +0700

    [Added]: register and getProfile service

--- app/services/user.js
+++ app/services/user.js
@@ -1,16 +1,24 @@
 import constants from '../config/constants';
 import firebase from './../config/firebase'
+import axios from 'axios';
 
 export default {
-    // getProfile: (uid) => fetch(constants.API_URL + '/user/' + uid, {
-    //     method: 'GET'
-    // }),
 
     getToken: () => {
         return firebase.auth().currentUser.getIdToken(true)
     },
 
-    test: () => {
-        return "KUY"
-    }
+    register: async (idToken, displayName) => {
+        return await axios.post(`${constants.API_URL}/regis`, {
+            user_token: idToken,
+            displayName: displayName
+        }).then(response => response.data)
+            .catch(error => error)
+    },
+
+    getProfile: async (uid) => {
+        return await axios.get(`${constants.API_URL}/user/${uid}`).then(response => response.data)
+            .catch(error => error)
+    },
+
 }
